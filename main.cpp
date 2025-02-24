59906891c3f77852235b652f119eefc34e728ec7
Lukasz Madry <lukaszmadry95@gmail.com>
2025-02-24 16:35:00 +0100

    params

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@
 #include <fstream>
 #include <array>
 #include <cmath>
+#include <unistd.h>
 using namespace std;
 
-// TODO : McKean-Vlasov
+// TODO : Davies-Harty dla fBm (bo obecne nie zadziala dla T > 1) ALBO przerobic Fouriera przez skalowanie T^H B_s =_{\PP} B_t dla s\in[0,1] i t \in[0,T] 
+// TODO : symulacje miary niezmienniczej w dlugim czasie (zrobione?)
 
 double my_unif(){
 	double number = rand();
@@ -41,7 +43,7 @@ double identity(double x){
 	return x;
 };
 
-// necessary?
+// necessary? is it possible to use templates or something like it ?
 double my_div(int k, double m){
 	return (double) k / m;
 };
@@ -54,11 +56,21 @@ double my_div(int m, int k){
 	return (double) m / (double) k;
 }
 
-double my_abs(double x){
+template <typename T>
+T my_abs(T x){
 	if (x > 0)
 		return x;
 	return -x;
 }
+
+template <typename T>
+T my_prod(std::vector<T> x){
+	int N, r(1);
+	N = x.size();
+	for (int i = 0; i <N; i++)
+		r *= x[i];
+	return r;
+};
 
 // here we are going to store matrices. We intend to keep them as 1d objects that will pretend to be 2D or 3D
 class Array{
@@ -68,6 +80,7 @@ public:
 	std::vector<int> dimensions;
 
 	// possible constructors:
+	Array();
 	Array(int);
 	Array(int, int);
 	Array(int, int, int);
@@ -75,11 +88,11 @@ public:
 	Array(int, int, std::vector<double>);
 	Array(int, int, int, std::vector<double>);
 
-	// general number of dimensions, although not sure why would I need it
+	// general number of dimensions
 	Array(std::vector<int>);
 	Array(std::vector<int>, std::vector<double>);
 
-	// basic operations with overloading go here
+	// getters
 	double get_value(int);
 	double get_value(int, int);
 	double get_value(int, int, int);
@@ -91,7 +104,7 @@ public:
 	void insert_value(int, std::vector<double>);
 
 	// debugging and output
-	void dump_to_textfile(std::string);
+	void dump_to_textfile(std::string, bool);
 	void print_first_values(int);
 
 	// operators
@@ -101,6 +114,12 @@ public:
 		return *this;
 	}
 };
+
+Array::Array(){
+	n_dim = 0;
+	this->dimensions = std::vector<int>();
+	this->data = std::vector<double>();
+}
 
 Array::Array(int size){
 	n_dim = 1;
@@ -119,6 +138,21 @@ Array::Array(int sizex, int sizey){
 		this->data.push_back(0);
 };
 
+Array::Array(std::vector<int> dim_vec){
+	if (dim_vec.size() == 1){
+		this->dimensions.push_back(dim_vec[0]);
+	} else if (dim_vec.size() == 2){
+		this->dimensions.push_back(dim_vec[0]);
+		this->dimensions.push_back(dim_vec[1]);
+	} else if (dim_vec.size() == 3){
+		this->dimensions.push_back(dim_vec[0]);
+		this->dimensions.push_back(dim_vec[1]);
+		this->dimensions.push_back(dim_vec[2]);
+	} else {
+		throw std::invalid_argument("dimension vector too large, currently we accept only up to three dimensions");
+	};
+};
+
 double Array::get_value(int i){
 	assert(i >= 0 && i < this->dimensions[0]);
 	return this->data[i];
@@ -126,12 +160,12 @@ double Array::get_value(int i){
 
 double Array::get_value(int line, int column){
 	return this->data[ this->dimensions[1] * line + column ];
-}
+};
 
 void Array::insert_value(int i, double val){
-	assert(i >= 0 && i < this->dimensions[0]);
+	assert(i >= 0 && i < this->dimensions[0] && this->n_dim == 1);
 	this->data[i] = val;
-}
+};
 
 void Array::insert_value(int i, int j, double val){
 	assert(this->n_dim == 2);
@@ -146,10 +180,15 @@ void Array::insert_value(int line, std::vector<double> values){
 	};
 };
 
-void Array::dump_to_textfile(std::string filename){
+void Array::dump_to_textfile(std::string filename, bool append){
 
 	ofstream im_file;
-	im_file.open(filename);
+
+	if (append)
+		im_file.open(filename, std::ios_base::app);
+	else
+		im_file.open(filename);
+
 	if (this->n_dim == 1){
 		for (int i = 0; i < this->data.size(); i++){
 			im_file << this->data[i] << " ";
@@ -163,6 +202,7 @@ void Array::dump_to_textfile(std::string filename){
 			im_file << endl;
 		};
 	};
+	im_file << endl;
 	im_file.close();
 };
 
@@ -184,7 +224,8 @@ double mean_tot(std::vector<double> a){
 	return my_div(m, tot);
 };
 
-double my_min(double x, double y){
+template <typename T>
+T my_min(T x, T y){
 	if (x<y)
 		return x;
 	return y;
@@ -193,12 +234,12 @@ double my_min(double x, double y){
 void Array::print_first_values(int number){
 
 	if (this->n_dim == 1){
-		for (int i = 0; i < my_min(number, this->data.size()); i++){
+		for (int i = 0; i < my_min<int>(number, this->data.size()); i++){
 			cout << this->data[i] << " ";
 		};
 	};
 	if (this->n_dim == 2) {
-		for (int i = 0; i < my_min(number, this->dimensions[0]); i++){
+		for (int i = 0; i < my_min<int>(number, this->dimensions[0]); i++){
 			for (int j = 0; j < this->dimensions[1]; j++){
 				cout << this->data[ i * this->dimensions[1] + j ] << " ";
 			};
@@ -216,17 +257,17 @@ Array sample_brownian_motion(int N){
 	for (int i = 1; i < N; i++){
 		values.insert_value(i, values.get_value(i-1) + timestep * my_gaussian());
 	};
-
 	return values;
 };
 
 // samples of fBm following Picard's "Representation formulae for fBm". Note that this can only work on the interval of [0,1]
 // TODO : write a simulation for an arbitrary interval so that you can simulate long-time behaviour
-Array sample_fbm_fourier(double hurst, int N, int cutoff){
+Array sample_fbm_fourier(double hurst, double timestep, int cutoff){
+	int N;
+	N = (int) 1/timestep;
 	Array values(N);
 	Array gaussians(cutoff, 2);
-	double timestep, power;
-	timestep = 1/(double) N;
+	double power;
 	power = hurst + 0.5;
 
 	// By Remark 6.7 in Picard's we can compute much simpler process than (77)
@@ -252,50 +293,66 @@ Array sample_fbm_fourier(double hurst, int N, int cutoff){
 	return values;
 };
 
-// create iterated integral, if possible maybe this could be a method in subclass Path
-// also in Path you could have sth like "get increment"
-// Array generate_roughpath(){
-
-// };
-
 // this function will take a function for the drift, the function for the noise and a path
 Array integrate_simple_sde(std::function<double(double)> drift, double volat, int N, Array path, double init_val = 0){
 	double timestep = 1/ (double) N;
 	double movement(0);
-	Array results(N);
-	results.insert_value(0, init_val);
+	if (path.n_dim == 1){
 
-	for (int i = 1; i < N; i++){
-		movement = drift(results.get_value(i-1)) * timestep + volat * (path.get_value(i)-path.get_value(i-1));
-		results.insert_value(i, results.get_value(i-1) + movement);
-	};
+		Array results(N);
+		results.insert_value(0, init_val);
 
-	return results;
+		for (int i = 1; i < N; i++){
+			movement = drift(results.get_value(i-1)) * timestep + volat * (path.get_value(i)-path.get_value(i-1));
+			results.insert_value(i, results.get_value(i-1) + movement);
+		};
+
+		return results;
+	} else if (path.n_dim == 2){
+
+		Array results(path.dimensions);
+
+	} else {
+		throw std::invalid_argument("path array has incorrect number of dimensions, expected 1 or 2");
+	}
 };
 
-Array integrate_sde_splitting(std::function<double(double, double)> flow, double volat, int N, Array path, double init_val = 0){
-	double timestep = my_div(1., N);
+Array integrate_sde_splitting(std::function<double(double, double, double, double)> flow, double volat, double timestep, 
+					double gamma, double drift_multip, Array &path, double init_val = 0){
 	double movement;
+	double val;
+	int N = (int)(1/timestep);
 	Array results(N);
 	results.insert_value(0, init_val);
 
 	for (int i = 1; i < N; i++){
 		movement = volat * (path.get_value(i)-path.get_value(i-1));
-		results.insert_value(i, flow(results.get_value(i-1) + movement, timestep));
+		val = results.get_value(i-1) + movement;
+		if (val > 0)
+			results.insert_value(i, flow(val, timestep, gamma, drift_multip));
+		else
+			results.insert_value(i, -flow(-val, timestep, gamma, 1));
 	};
 	return results;
 };
 
 // to be rewritten with new noises ? we can easily add drift as well 
-Array integrate_ips_sde(std::function<double(double, double)> interaction, int Ninter, int Nsteps, double mult = 1, int report_progress = 100){
-	double timestep = 1 / (double) Nsteps;
+Array integrate_ips_sde(std::function<double(double, double, double)> interaction, int Ninter, 
+	double timestep, double mult = 1, int report_progress = 100, double g = -0.5, double hurst=0.5, bool bm=1, int cutoff = 1000){
+	int Nsteps;
 
 	// first we need to generate noises
 	Array noises(Ninter, Nsteps);
 	Array values(Ninter, Nsteps);
-	for (int i = 0; i < Ninter; i++){
-		noises.insert_value(i, sample_brownian_motion(Nsteps).data);
-	};
+	if (bm){
+		for (int i = 0; i < Ninter; i++){
+			noises.insert_value(i, sample_brownian_motion(Nsteps).data);
+		};
+	} else{
+		for (int i = 0; i < Ninter; i++){
+			noises.insert_value(i, sample_fbm_fourier(hurst, timestep, cutoff));
+		}
+	}
 
 	noises *= mult;
 
@@ -306,7 +363,7 @@ Array integrate_ips_sde(std::function<double(double, double)> interaction, int N
 			double movement(0);
 			for (int j = 0; j < Ninter; j++){
 				if (j != i){
-					movement += interaction(values.get_value(i, t-1), values.get_value(j, t-1)) * timestep;
+					movement += interaction(values.get_value(i, t-1), values.get_value(j, t-1), g) * timestep;
 				};
 			};
 			movement = my_div(movement, Ninter-1);
@@ -400,57 +457,74 @@ double integrate_montecarlo(int N, int Nint, function<double(Array)> f, function
 	return mean_tot(integrate_montecarlo_return_all(N, Nint, f, drift));
 };
 
-// 
-
-// dumb functions created to be passed as arguments
-
 // value of -0.5 is hardcoded, not sure how to make it passable atm, will check
-double power_fn(double x){
+double power_fn(double x, double g){
 	if (my_abs(x) < 0.001)
 		return 0;
 	if (x > 0)
-		return my_div(1, pow(my_abs(x), 0.5));
-	return -my_div(1, pow(my_abs(x), 0.5));
+		return my_div(1, pow(my_abs(x), g));
+	return -my_div(1, pow(my_abs(x), g));
 };
 
-double interaction_power(double x, double y){
-	return power_fn(x-y);
-}
+double interaction_power(double x, double y, double g){
+	return power_fn(x-y, g);
+};
 
 // it stems from x_t = ( x_0^(1-g) + t)^(1/(1-g)), this one corresponds to g = -1/2, but I plan on extending it to variable g
-double flow_fn(double x, double h){
-	return pow( pow(x, 1.5) + h, 0.67 );
+double flow_fn(double x, double h, double g, double multip){
+	return pow( pow(x, 1-g) + h, multip * my_div(1, 1-g) );
 };
 
-int main(){
+void run_zero_noise(int N, double H, double g, double timestep, double eps, double Amult, int cutoff, std::string result_fname){
+	Array noise, sde;
+	int count_positive;
+	int length = (int)(1/timestep);
+	for (int i = 0; i < N; i++){
+		noise = sample_fbm_fourier(H, timestep, cutoff);
+		sde = integrate_sde_splitting(flow_fn, eps, timestep, 
+					g, Amult, noise);
+		sde.dump_to_textfile(result_fname, 1);
+		if (sde.get_value(length-1) > 0)
+			count_positive++;
+	};
+};
+
+int main(int argc, char** argv){
 	srand((unsigned) time(NULL));
 
-	int Ninter(10000), Nsteps(1000);
-	Array values = integrate_ips_sde(interaction_power, Ninter, Nsteps, 0.01);
+	double g, timestep, eps(1), H, Amult;
+	int N(100), cutoff(1000);
+	bool interacting_system(0);
+	std::string fname;
 
-	values.dump_to_textfile("result_ips.txt");
+	for (int i = 1; i < argc; i++){
+		if (std::string(argv[i]) == "-g")
+			g = atof( argv[i+1]);
+		if (std::string(argv[i]) == "-eps")
+			eps = atof( argv[i+1]);
+		if (std::string(argv[i]) == "-ts")
+			timestep = atof(argv[i+1]);
+		if (std::string(argv[i]) == "-H")
+			H = atof(argv[i+1]);
+		if (std::string(argv[i]) == "-N")
+			N = atoi(argv[i+1]);
+		if (std::string(argv[i]) == "--ips")
+			interacting_system = 1;
+		if (std::string(argv[i]) == "-A")
+			Amult = atof(argv[i+1]);
+		if (std::string(argv[i]) == "-cutoff")
+			cutoff = atoi(argv[i+1]);
+		if (std::string(argv[i]) == "-fname")
+			fname = argv[i+1];
+	};
 
+	cout << "gamma " << g << ", timestep " << timestep << ", H " << H << ", eps " << eps << ", N " << N << ", cutoff " << cutoff << endl;
 
-
-	// Array u = sample_brownian_motion(1000);
-	// Array t = sample_fbm_fourier(0.25, 1000, 1000);
-	// Array result(1000);
-	// t.dump_to_textfile("fbm.txt");
-	// u.dump_to_textfile("bm.txt");
-	// double epsilon = 1;
-	// std::string filename;
-
-	// for (int e = 1; e < 4; e++){
-	// 	epsilon = my_div(1, pow(10, e-1));
-	// 	cout << "considering " << epsilon << endl;
-	// 	cout << "power_fn test" << power_fn(0) << endl;
-	// 	result = integrate_simple_sde(power_fn, epsilon, 1000, u); // double volat, int N, Array path, double init_val = 0
-	// 	filename = "integr_simple_" + std::to_string(e) + ".txt";
-	// 	cout << endl;
-	// 	result.dump_to_textfile(filename);
-
-	// 	result = integrate_sde_splitting(flow_fn, epsilon, 1000, u);
-	// 	filename = "integr_split_" + std::to_string(e) + ".txt";
-	// 	result.dump_to_textfile(filename);
-	
+	if (interacting_system){
+		Array interactions = integrate_ips_sde(interaction_power, N, 
+			timestep, eps, N / 10, g, H, cutoff);
+		interactions.dump_to_textfile(fname, 0);
+	} else {
+		run_zero_noise(N, H, g, timestep, eps, Amult, cutoff, fname);
+	}
 };
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 clang++ -std=c++11 -g main.cpp -o main.bin
-./main.bin
+./main.bin -N 100 -fname results.txt -A 20  -g -0.5 -H 0.25 -eps 0.001 -ts 0.001
